19e93ec65194f53ea49b53484038bdf3267ecf1e
Ck T <chengkou@gmail.com>
2019-04-06 11:54:39 -0500

    add my info

---
+++ config/website.js
@@ -0,0 +1,25 @@
+const tailwind = require('../tailwind')
+
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+
+  siteTitle: 'Ck - Chengkou Portfolio', // Navigation and Site Title
+  siteTitleAlt: 'Ck', // Alternative Site title for SEO
+  siteTitleShort: 'Ck', // short_name for manifest
+  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
+  siteUrl: 'https://chengkou.dev', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/sun.jpg', // Used for SEO and manifest
+  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
+  author: 'Ck', // Author for schema.org JSONLD
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  // userTwitter: '@cara', // Twitter Username
+  // ogSiteName: 'cara', // Facebook Site Name
+  ogLanguage: 'en_US', // Facebook Language
+  googleAnalyticsID: 'UA-47519312-5',
+
+  // Manifest and Progress color
+  themeColor: tailwind.colors.orange,
+  backgroundColor: tailwind.colors.blue,
+}
---
+++ gatsby-config.js
@@ -0,0 +1,57 @@
+const config = require('./config/website')
+
+const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
+
+module.exports = {
+  /* General Information */
+  siteMetadata: {
+    siteUrl: config.siteUrl + pathPrefix,
+  },
+  /* Plugins */
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/images/`,
+        name: 'images',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: config.googleAnalyticsID,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: config.siteTitle,
+        short_name: config.siteTitleShort,
+        description: config.siteDescription,
+        start_url: config.pathPrefix,
+        background_color: config.backgroundColor,
+        theme_color: config.themeColor,
+        display: 'standalone',
+        icons: [
+          {
+            src: '/favicons/sun.jpg',
+            sizes: '192x192',
+            type: 'image/jpg',
+          },
+          {
+            src: '/favicons/sun.jpg',
+            sizes: '512x512',
+            type: 'image/jpg',
+          },
+        ],
+      },
+    },
+    /* Must be placed at the end */
+    'gatsby-plugin-offline',
+    'gatsby-plugin-netlify',
+  ],
+}
